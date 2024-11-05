7da5d26f7c71542780289092abd2ef72c277c787
Gunjae Yoon <gjyoon@plaif.com>
2024-11-05 16:16:44 +0900

    Update event-handling-benchmark.cmake to include generator.cpp

--- cmake/event-handling-benchmark.cmake
+++ cmake/event-handling-benchmark.cmake
@@ -2,6 +2,7 @@ set(CMAKE_TARGET_NAME "event-handling-benchmark")
 
 add_executable(${CMAKE_TARGET_NAME}
 	src/cpp/app/main.cpp
+	src/cpp/tester/base/generator.cpp
 	src/cpp/tester/impl/epoll.cpp
 	src/cpp/tester/impl/multithreading.cpp
 	src/cpp/tester/impl/select.cpp
