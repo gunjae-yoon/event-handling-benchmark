0976a476867efc33e6a8d944d3477c9139b67e18
Gunjae Yoon <gjyoon@plaif.com>
2024-11-05 14:20:54 +0900

    Move main.cpp to src/cpp/app/ from src/

--- cmake/event-handling-benchmark.cmake
+++ cmake/event-handling-benchmark.cmake
@@ -1,11 +1,11 @@
 set(CMAKE_TARGET_NAME "event-handling-benchmark")
 
 add_executable(${CMAKE_TARGET_NAME}
-	src/main.cpp
+	src/cpp/app/main.cpp
 )
 
 target_include_directories(${CMAKE_TARGET_NAME} PRIVATE
-	src
+	src/cpp
 )
 
 target_link_libraries(${CMAKE_TARGET_NAME} PRIVATE
