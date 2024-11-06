95e09b09b5e67a15c68f6d74db25c03d11ae72d9
Gunjae Yoon <gjyoon@plaif.com>
2024-11-06 10:09:16 +0900

    Implement data generator

--- src/cpp/tester/base/generator.cpp
+++ src/cpp/tester/base/generator.cpp
@@ -1,19 +1,70 @@
-#include "tester/base/generator.h"
+#include "generator.h"
+#include <stdexcept>
+#include <iostream>
 
 namespace event_benchmark {
-	Generator::Generator() {
+	Generator::Generator() :
+		manager(nullptr),
+		is_running(false) {
 	}
 
 	Generator::~Generator() {
+		stop();
+		manager.reset();
 	}
 
-	bool Generator::construct_test_environment(const uint64_t count) {
+	bool Generator::construct_test_environment(std::shared_ptr<MqManager> mq_manager) {
+		if (is_running.load() && running_thread.joinable()) {
+			throw std::runtime_error("The data generator is busy");
+		}
+		
+		manager = mq_manager;
 		return true;
 	}
 
 	void Generator::measure_reaction_time() {
+		if (is_running.load()) {
+			return;
+		}
+		
+		is_running.store(true);
+		running_thread = std::thread([this]() {
+			while (is_running) {
+				for (mq_desc& desc : manager->queues) {
+					std::chrono::nanoseconds now = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now().time_since_epoch());
+					uint64_t count = now.count();
+					if (mq_send(desc.second, reinterpret_cast<const char*>(&count), sizeof(uint64_t), 0) == -1) {
+						std::cerr << "Failed to send message to " << desc.first << std::endl;
+					}
+					std::this_thread::sleep_for(std::chrono::milliseconds(100));
+				}
+			};
+		});
 	}
 
 	void Generator::measure_throughput() {
+		if (is_running.load()) {
+			return;
+		}
+		
+		is_running.store(true);
+		running_thread = std::thread([this]() {
+			uint64_t count = 0;
+			while (is_running) {
+				count++;
+				for (mq_desc& desc : manager->queues) {
+					if (mq_send(desc.second, reinterpret_cast<const char*>(&count), sizeof(uint64_t), 0) == -1) {
+						std::cerr << "Failed to send message to " << desc.first << std::endl;
+					}
+				}
+			};
+		});
+	}
+
+	void Generator::stop() noexcept {
+		is_running.store(false);
+		if (running_thread.joinable()) {
+			running_thread.join();
+		}
 	}
 }
