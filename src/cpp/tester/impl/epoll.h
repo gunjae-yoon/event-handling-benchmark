6158024d5e74f242b2695c25914f1263cd874230
Gunjae Yoon <gjyoon@plaif.com>
2024-11-08 16:33:52 +0900

    Update members of epoll and multithreading

--- src/cpp/tester/impl/epoll.h
+++ src/cpp/tester/impl/epoll.h
@@ -2,6 +2,8 @@
 #define __event_handling_benchmark_tester_impl_epoll_h__
 
 #include "tester/base/tester.h"
+#include <list>
+#include <mutex>
 
 namespace event_benchmark {
 	namespace epoll {
@@ -20,6 +22,18 @@ namespace event_benchmark {
 			
 			[[nodiscard]] ReactionTime get_reaction_time() override;
 			[[nodiscard]] Throughput get_throughput() override;
+			
+		private:
+			struct {
+				std::list<uint64_t> results;
+				std::mutex mutex;
+				ReactionTime statistics;
+			} reaction_time;
+			struct {
+				std::list<uint64_t> results;
+				std::mutex mutex;
+				Throughput statistics;
+			} throughput;
 		};
 	}
 }
