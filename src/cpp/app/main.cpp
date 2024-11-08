e0714b7996f52ef3f6d90cd8ec6e16d5d2a04176
Gunjae Yoon <gjyoon@plaif.com>
2024-11-08 16:32:08 +0900

    Implement epoll test performing code in main.cpp

--- src/cpp/app/main.cpp
+++ src/cpp/app/main.cpp
@@ -12,12 +12,104 @@
 using namespace event_benchmark;
 using namespace std::chrono_literals;
 
+void test_reaction_time_of_epoll(const uint64_t count, const uint64_t size);
+void test_throughput_of_epoll(const uint64_t count, const uint64_t size);
 void test_reaction_time_of_multithreading(const uint64_t count, const uint64_t size);
 void test_throughput_of_multithreading(const uint64_t count, const uint64_t size);
 
 int main(int argc, char** argv) {
+	test_reaction_time_of_epoll(10, 1024);
+	test_throughput_of_epoll(10, 1024);
 	test_reaction_time_of_multithreading(10, 1024);
 	test_throughput_of_multithreading(10, 1024);
+}
+
+void test_reaction_time_of_epoll(const uint64_t count, const uint64_t size) {
+	std::shared_ptr<MqManager> manager = std::make_shared<MqManager>();
+	if (manager->reset(count, size) == false) {
+		manager.reset();
+		return;
+	}
+
+	pid_t pid = fork();
+	if (pid < 0) { // error
+		return;
+	} else if (pid == 0) { // child process
+		Generator generator;
+		if (generator.construct_test_environment(manager) == false) {
+			return;
+		}
+		
+		std::this_thread::sleep_for(100ms);
+		generator.measure_reaction_time();
+		std::this_thread::sleep_for(10s);
+		generator.stop();
+		manager.reset();
+		exit(0);
+	} else { // parent process
+		epoll::TesterImpl tester;
+		if (tester.construct_test_environment(manager) == false) {
+			manager->dispose();
+			return;
+		}
+		
+		tester.measure_reaction_time();
+		Tester::ReactionTime result = tester.get_reaction_time();
+		
+		manager->dispose();
+		
+		waitpid(pid, nullptr, 0);
+		
+		std::cout << "[epoll]" << std::endl;
+		std::cout << "    minimum reaction time: " << result.minimum.count() << " ns" << std::endl;
+		std::cout << "    maximum reaction time: " << result.maximum.count() << " ns" << std::endl;
+		std::cout << "    average reaction time: " << result.average.count() << " ns" << std::endl;
+		manager.reset();
+	}
+}
+
+void test_throughput_of_epoll(const uint64_t count, const uint64_t size) {
+	std::shared_ptr<MqManager> manager = std::make_shared<MqManager>();
+	if (manager->reset(count, size) == false) {
+		manager.reset();
+		return;
+	}
+
+	pid_t pid = fork();
+	if (pid < 0) { // error
+		return;
+	} else if (pid == 0) { // child process
+		Generator generator;
+		if (generator.construct_test_environment(manager) == false) {
+			return;
+		}
+		
+		std::this_thread::sleep_for(100ms);
+		generator.measure_throughput();
+		std::this_thread::sleep_for(10s);
+		generator.stop();
+		manager.reset();
+		exit(0);
+	} else { // parent process
+		epoll::TesterImpl tester;
+		if (tester.construct_test_environment(manager) == false) {
+			manager->dispose();
+			return;
+		}
+		
+		tester.measure_throughput();
+		Tester::Throughput result = tester.get_throughput();
+		
+		manager->dispose();
+		
+		waitpid(pid, nullptr, 0);
+		
+		std::cout << "[epoll]" << std::endl;
+		std::cout << "    minimum throhgput: " << result.minimum << " dps" << std::endl;
+		std::cout << "    maximum throhgput: " << result.maximum << " dps" << std::endl;
+		std::cout << "    average throhgput: " << result.average << " dps" << std::endl;
+		manager.reset();
+	}
 }
 
 void test_reaction_time_of_multithreading(const uint64_t count, const uint64_t size) {
--- src/cpp/tester/testers.h
+++ src/cpp/tester/testers.h
@@ -1,3 +1,4 @@
 #include "tester/base/mq_manager.h"
 #include "tester/base/generator.h"
+#include "tester/impl/epoll.h"
 #include "tester/impl/multithreading.h"
